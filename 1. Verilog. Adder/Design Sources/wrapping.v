318709df9f0411a966aee08b3c4419584a003333
Polina <polina.kleiner@gmail.com>
2022-10-13 16:09:13 +0300

    edited wrapping

--- 1. Verilog. Adder/Design Sources/wrapping.v
+++ 1. Verilog. Adder/Design Sources/wrapping.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.10.2022 14:32:32
-// Design Name: 
-// Module Name: wrapping
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module wrapping(
                input [15:0] SW,
